133859ba4aba623ca78ea544e9f9b0c0915307cd
DYGITArchitect <DYGITArchitect@gmail.com>
2022-11-22 13:14:48 +0100

    Add post router and controller

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import Post from "./Post.js";
+import router from "./router.js";
 
 const PORT = 5000;
 const DB_URL =
@@ -8,16 +9,7 @@ const DB_URL =
 
 const app = express();
 app.use(express.json());
-
-app.post("/", async (req, res) => {
-  try {
-    const { author, title, content, picture } = req.body;
-    const post = await Post.create({ author, title, content, picture });
-    res.json(post);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+app.use("/api", router);
 
 async function startApp() {
   try {
