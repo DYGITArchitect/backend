91e3ff7041ff6b1ee80cf313c22727b6f7f3bc28
DYGITArchitect <DYGITArchitect@gmail.com>
2022-12-05 15:41:51 +0100

    changed bcrypt to bcryptjs

--- server/services/UserService.js
+++ server/services/UserService.js
@@ -1,5 +1,5 @@
 import User from "../models/User.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { v4 as uuid } from "uuid";
 import mailService from "../services/MailService.js";
 import tokenService from "./TokenService.js";
